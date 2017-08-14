6fb78f691f27953435295c8a6c9f9929548aec66
Henrique <henrique@mobilesys.com.br>
2017-08-14 09:38:29 -0300

    - atualizacao do pom.

--- Exemplo/src/main/java/br/com/exemplo/filter/FiltroAltenticacao.java
+++ Exemplo/src/main/java/br/com/exemplo/filter/FiltroAltenticacao.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -14,8 +15,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebFilter("/paginas/*")
 public class FiltroAltenticacao implements Filter {
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest hreq = (HttpServletRequest) request;
 		HttpServletResponse hres = (HttpServletResponse) response;
 		Object user = hreq.getSession().getAttribute("usuario");
@@ -24,6 +29,12 @@ public class FiltroAltenticacao implements Filter {
 			return;
 		}
 		chain.doFilter(request, response);
+
+	}
+
+	public void init(FilterConfig filter) throws ServletException {
+		// TODO Auto-generated method stub
+
 	}
 
 }
